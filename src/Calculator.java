08a44007f688ae430023b1c69aed086c5002bcd5
Aleksandr Petrenko <petrenko.aleksandr025@gmail.com>
2016-09-09 00:32:11 +0300

    2 переменные-один объект

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+
+public class Calculator {
+	int a;
+	float s;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator b = new Calculator();
+		b.a = 4;
+		b.s = 320.f;
+		System.out.println("b.a = " + b.a);
+		System.out.println("b.s = " + b.s);
+		
+
+	}
+
+}
